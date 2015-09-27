6d3d50dc3f84490e0a057d8ef7ded92882415a0b
Rafael Rendón Pablo <rafael@intangible.mx>
2015-09-26 20:35:54 -0500

    Some refactoring

--- lib/settings.rb
+++ lib/settings.rb
@@ -1,3 +1,4 @@
-class Settings
+# This module contains constants used by the Twitter client.
+module Settings
   MAX_FOLLOWER_IDS = 5000
 end
--- lib/twitter_client.rb
+++ lib/twitter_client.rb
@@ -2,60 +2,57 @@ require 'rest_client'
 require 'open-uri'
 require 'base64'
 
-BASE_URL = "https://api.twitter.com/1.1"
+BASE_URL = 'https://api.twitter.com/1.1'
 
+# This is the Twitter client class that will allow you to query the Twitter API.
 class TwitterClient
   attr_accessor :consumer_key
   attr_accessor :consumer_secret
 
   def self.bearer_token(ck, cs)
-    ck = URI::encode(ck)
-    cs = URI::encode(cs)
+    ck = URI.encode(ck)
+    cs = URI.encode(cs)
     Base64.strict_encode64(ck + ':' + cs)
   end
 
   def self.access_token(ck, cs)
     bearer_token = TwitterClient.bearer_token(ck, cs)
     begin
-    resp = RestClient::Request.execute({
-      method: :post,
-      url: 'https://api.twitter.com/oauth2/token',
-      headers: {
-        'User-Agent' => 'My twitter App',
-        'Authorization' => "Basic #{bearer_token}",
-        'Content-Type' => 'application/x-www-form-urlencoded;charset=UTF-8',
-        'Content-Length' => 29,
-        'Accept-Encoding' => 'gzip',
-      },
-      payload: 'grant_type=client_credentials',
-    })
-    JSON.parse(resp)['access_token']
+      resp = RestClient::Request.execute(
+        method: :post,
+        url: 'https://api.twitter.com/oauth2/token',
+        headers: {
+          'User-Agent' => 'My twitter App',
+          'Authorization' => "Basic #{bearer_token}",
+          'Content-Type' => 'application/x-www-form-urlencoded;charset=UTF-8',
+          'Content-Length' => 29,
+          'Accept-Encoding' => 'gzip'
+        },
+        payload: 'grant_type=client_credentials'
+      )
+      JSON.parse(resp)['access_token']
     rescue => e
-      if e.http_code == 403
-        raise AuthenticationError
-      else
-        raise e
-      end
+      raise e.http_code == 403 ? AuthenticationError : e
     end
   end
 
   def self.test_access(ck, cs)
-    return !self.access_token(ck, cs).empty?
+    !access_token(ck, cs).empty?
   end
 
   def follower_ids(user_id, count = 5000)
     access_token = TwitterClient.access_token(consumer_key, consumer_secret)
     url = "#{BASE_URL}/followers/ids.json?count=#{count}&user_id=#{user_id}"
-    resp = RestClient::Request.execute({
+    resp = RestClient::Request.execute(
       method: :get,
       url: url,
       headers: {
         'User-Agent'      => 'My Twitter App',
         'Authorization'   => "Bearer #{access_token}",
-        'Accept-Encoding' =>  'gzip',
+        'Accept-Encoding' =>  'gzip'
       }
-    })
-   JSON.parse(resp)['ids']
+    )
+    JSON.parse(resp)['ids']
   end
 
   def initialize(ck, cs)
@@ -63,12 +60,3 @@ class TwitterClient
     @consumer_secret = cs
   end
 end
-
-#ck = ENV['CONSUMER_KEY']
-#cs = ENV['CONSUMER_SECRET']
-#client = TwitterClient.new(ck, cs)
-
-# Retrieve the first 5000 follower IDs of @SGgrc
-#client.follower_ids(140162079).each do |id|
-#  puts id
-#end
